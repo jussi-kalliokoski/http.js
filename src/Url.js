018d0983e840b61d4360f14200080962d03324be
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2014-05-19 15:38:44 +0300

    Implemented Url.parse().

--- src/Url.js
+++ src/Url.js
@@ -4,6 +4,8 @@ Http.Url = function () {
     function Url () {
     }
 
+    Url.location = location;
+
     var encodeParameter = function (key, value) {
         return key + "=" + encodeURIComponent(value);
     };
@@ -18,6 +20,63 @@ Http.Url = function () {
 
             return encodeParameter(key, value);
         })).join("&");
+    };
+
+    /*
+     * Collected backreferences:
+     * $1: origin
+     * $2: protocol
+     * $3: host
+     * $4: hostname
+     * $5: port
+     * $6: pathname
+     * $7: search
+     * $8: hash
+    */
+    var urlParserRegExp = /^(((?:\w+):)\/\/(([^:\/\s\?\#]+)(?::(\d+))?))?((?:\/\w+)*\/(?:[\w\-\.]+[^\#?\s]+)?)?(\?[^\#]*)?(\#.*)?$/;
+    var urlDefaults = {
+        href: "",
+        protocol: "",
+        host: "",
+        hostname: "",
+        port: "",
+        pathname: "/",
+        search: "",
+        hash: "",
+        origin: ""
+    };
+
+    Url.parse = function (url) {
+        if ( !urlParserRegExp.test(url) ) {
+            throw new Error("Invalid URL");
+        }
+
+        var parsed = {
+            href: RegExp["$&"],
+            origin: RegExp.$1,
+            protocol: RegExp.$2,
+            host: RegExp.$3,
+            hostname: RegExp.$4,
+            port: RegExp.$5,
+            pathname: RegExp.$6,
+            search: RegExp.$7,
+            hash: RegExp.$8
+        };
+
+        _.each(parsed, function (value, key) {
+            parsed[key] = value || urlDefaults[key];
+        });
+
+        if ( !parsed.origin ) {
+            return Url.parse(
+                Url.parse(Url.location.href).origin +
+                parsed.pathname +
+                parsed.search +
+                parsed.hash
+            );
+        }
+
+        return parsed;
     };
 
     return Url;
--- test/UrlSpec.js
+++ test/UrlSpec.js
@@ -30,4 +30,42 @@ describe("Http.Url", function () {
             Url.encodeQuery(data).should.equal("x[]=foo&x[]=bar");
         });
     });
+
+    describe(".parse()", function () {
+        it("should parse the URL correctly", function () {
+            var url = Url.parse("https://foo.example.com:912/bar?q=cat&x=dog#section-1");
+            url.protocol.should.equal("https:");
+            url.host.should.equal("foo.example.com:912");
+            url.hostname.should.equal("foo.example.com");
+            url.port.should.equal("912");
+            url.pathname.should.equal("/bar");
+            url.search.should.equal("?q=cat&x=dog");
+            url.hash.should.equal("#section-1");
+            url.origin.should.equal("https://foo.example.com:912");
+        });
+
+        it("should not add the port to origin if it's not present", function () {
+            var url = Url.parse("https://foo.example.com/bar?q=cat&x=dog#section-1");
+            url.origin.should.equal("https://foo.example.com");
+        });
+
+        it("should default origin to the current origin", function () {
+            var url = Url.parse("/asd");
+            url.origin.should.not.equal("");
+        });
+
+        it("should work with URLs with just `/` as pathname", function () {
+            var url = Url.parse("http://foo.bar/");
+            url.pathname.should.equal("/");
+        });
+
+        it("should throw if the string is not a valid URL", function () {
+            var fn = function () {
+                Url.parse("~");
+            };
+
+            fn.should.throw(Error);
+            fn.should.throw("Invalid URL");
+        });
+    });
 });
