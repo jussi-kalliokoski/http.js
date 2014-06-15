e6406f864f0630f53640e264b59610c0a423ca06
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2014-06-16 00:40:40 +0300

    Added a response type handler for HTML.
    Closes #9.

---
+++ test/ResponseTypeHandlers/htmlSpec.js
@@ -0,0 +1,34 @@
+describe("Http.Request (html response type handler)", function () {
+    "use strict";
+
+    var server;
+
+    beforeEach(function () {
+        server = sinon.fakeServer.create();
+        server.autoRespond = true;
+    });
+
+    afterEach(function () {
+        server.restore();
+    });
+
+    var defaultResponse = "<div>Foo</div>";
+
+    it("should send the request to the given URL", function () {
+        server.respondWith("GET", "http://otherdomain.com/foo", function (xhr) {
+            expect(xhr.requestHeaders.Accept).to.equal("text/html");
+            xhr.respond(200, {
+                "Content-Type": "text/html"
+            }, defaultResponse);
+        });
+
+        return Http.get({
+            url: "http://otherdomain.com/foo",
+            responseType: "html"
+        }).then(function (result) {
+            expect(result.statusCode).to.equal(200);
+            expect(result.headers["Content-Type"]).to.equal("text/html");
+            expect(result.body.innerHTML).to.match(/<div>foo<\/div>/i);
+        });
+    });
+});
